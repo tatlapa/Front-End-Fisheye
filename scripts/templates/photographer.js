63357ca9d5d15aa733b3f938b9a1dc87dace48f3
tatlapa <49989876+tatlapa@users.noreply.github.com>
2023-12-10 19:23:46 +0700

    data send + data display + modif css page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,30 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `../../assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+    const link = document.createElement("a");
+
+    img.setAttribute("src", picture);
+    h2.textContent = name;
+    h3.textContent = `${city}, ${country}`;
+    h4.textContent = tagline;
+    p.textContent = price + "€/jour";
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(p);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
