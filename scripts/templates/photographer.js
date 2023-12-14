a0fb1fcbb9be8a0400c1b72cea5d61d4ea68054d
tatlapa <49989876+tatlapa@users.noreply.github.com>
2023-12-14 10:49:19 +0700

    index terminé

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,29 +4,48 @@ function photographerTemplate(data) {
   const picture = `../../assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
+ 
+    // Création des balises html
     const article = document.createElement("article");
+    const link = document.createElement("a");
     const img = document.createElement("img");
     const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
-    const h4 = document.createElement("h4");
-    const p = document.createElement("p");
+    const location = document.createElement("p");
+    const description = document.createElement("p");
+    const dayPrice = document.createElement("span");
 
+    // Attribution des labels et des classes
     img.setAttribute("src", picture);
     img.setAttribute("alt", "Image du photographe");
     h2.textContent = name;
     h2.setAttribute("aria-label", "Nom du photographe");
-    h3.textContent = `${city}, ${country}`;
-    h3.setAttribute("aria-label", "Ville et pays du photographe");
-    h4.textContent = tagline;
-    h4.setAttribute("aria-label", "Description du photographe");
-    p.textContent = price + "€/jour";
-    p.setAttribute("aria-label", "Prix du photographe");
+    location.textContent = `${city}, ${country}`;
+    location.setAttribute("aria-label", "Ville et pays du photographe");
+    location.classList.add("location");
+    description.textContent = tagline;
+    description.setAttribute("aria-label", "Description du photographe");
+    description.classList.add("description");
+    dayPrice.textContent = price + "€/jour";
+    dayPrice.setAttribute("aria-label", "Prix du photographe");
+    link.setAttribute("aria-role", "lien vers page du photographe");
+    link.setAttribute("aria-label", `lien vers page du photographe + ${name}`);
 
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(h3);
-    article.appendChild(h4);
-    article.appendChild(p);
+    // Ordre des balises html
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(description);
+    article.appendChild(dayPrice);
+
+    // Gestionnaire d'événements au lien
+    link.addEventListener("click", function(event) {
+      event.preventDefault(); // Pour éviter que le lien ne change directement l'URL
+
+      // Mise à jour de l'URL avec l'ID du photographe
+      const newUrl = window.location.origin + `/photographer.html?id=${id}`;
+      window.location.href = newUrl;
+    });
 
     return article;
   }
