e3c4ce5e02c776f88ee95d66abd6e9f768fdd978
tatlapa <49989876+tatlapa@users.noreply.github.com>
2023-12-29 09:57:51 +0700

    current media lightbox ok

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,20 +1,49 @@
 export const displayLightBox = (medias, photographer) => {
     const lightBoxContainer = document.getElementById('lightbox');
+    let currentMediaIndex = 0; 
 
-    // Ouverture lightBox
+    // Sélectionne tous les éléments ayant la classe '.open-light-box'
     const openLightBoxes = document.querySelectorAll('.open-light-box');
-    openLightBoxes.forEach(openLightBox => {
+    openLightBoxes.forEach((openLightBox, index) => {
+        // Ajoute un écouteur de clic à chaque élément '.open-light-box'
         openLightBox.addEventListener("click", () => {
+            // Lorsqu'un élément est cliqué, affiche la lightbox et met à jour l'index du média actuel
             lightBoxContainer.style.display = "flex";
+            currentMediaIndex = index; 
+            // Appelle la fonction pour afficher le média correspondant à l'index actuel
+            displayCurrentMedia(currentMediaIndex, medias, photographer); 
         });
     });
 
-    //Affichage du média correspondant
-    
-
-    // Fermeture lightBox
+    // Ferme la lightbox lorsqu'on clique sur le bouton de fermeture
     const btnClose = document.querySelector(".btn_lightBox_close");
     btnClose.addEventListener("click", () => {
         lightBoxContainer.style.display = "none";
     });
+};
+
+// Fonction pour afficher le média correspondant à l'index donné
+const displayCurrentMedia = (index, medias, photographer) => {
+    const lightBoxMedia = document.querySelector('.lightBox_media');
+    const currentMedia = medias[index]; // Récupère le média correspondant à l'index
+    
+    // Nettoie le contenu précédent de la lightbox
+    while (lightBoxMedia.firstChild) {
+        lightBoxMedia.removeChild(lightBoxMedia.firstChild);
+    }
+
+    // Ajoute le contenu du média à la lightbox en fonction de son type (image ou vidéo)
+    if (currentMedia.image) {
+        const imageElement = document.createElement('img');
+        imageElement.src = `./assets/media/${photographer.name}/${currentMedia.image}`;
+        imageElement.alt = currentMedia.title;
+        lightBoxMedia.appendChild(imageElement);
+    } else if (currentMedia.video) {
+        const videoElement = document.createElement('video');
+        videoElement.src = `./assets/media/${photographer.name}/${currentMedia.video}`;
+        videoElement.setAttribute('controls', true);
+        videoElement.setAttribute('type', 'video/mp4');
+        videoElement.innerHTML = "Your browser does not support the video tag.";
+        lightBoxMedia.appendChild(videoElement);
+    }
 };
